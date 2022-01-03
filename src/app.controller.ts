cc073e7838e9d482034f7f44c91696a9fd8a3a40
Daniel Correa <yo@danielgara.com>
2022-01-03 16:53:21 -0500

    imp: chapter 07-2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,22 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get("/")
   @Render('index')
-  index() {}
+  index() {
+    return { 
+      title: "Home Page - Online Store"
+    };
+  }
+
+  @Get("/about")
+  @Render('about')
+  about() {
+    let viewData = [];
+    viewData["description"] = "This is an about page ...";
+    viewData["author"] = "Developed by: Your Name";
+    return { 
+      title: "About us - Online Store",
+      subtitle: "About us",
+      viewData: viewData
+    };
+  }
 }
